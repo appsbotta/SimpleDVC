27fbbaca8a6fbca74cb1de984accb37738557dd1
Lokesh <appsbotta@gmail.com>
2025-02-23 11:32:20 +0530

    App created

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 import yaml
 import joblib
 import numpy as np
+from src.get_data import read_params
 
 params_path = "params.yaml"
 webapp_root = "webapp"
@@ -13,9 +14,44 @@ template_dir = os.path.join(webapp_root,"templates")
 
 app = Flask(__name__,static_folder=static_dir,template_folder=template_dir)
 
+
+def predict(data):
+    config = read_params(params_path)
+    model_dir_path = config["webapp_model_dir"]
+    model = joblib.load(model_dir_path)
+    prediction = model.predict(data)
+    return prediction[0]
+
+
+def api_response(request):
+    try:
+        data = np.array([list(request.json.values())])
+        response = predict(data)
+        response = {"response":response}
+        return response
+    except Exception as e:
+        print(e)
+        error = {"error" : "Something Went wrong try again"}
+        return render_template("404.html",error=error)
+
+
+
 @app.route('/',methods=["GET","POST"])
 def index():
     if request.method == "POST":
+        try:
+            if request.form:
+                data = dict(request.form).values()
+                data = [list(map(float,data))]
+                response = predict(data)
+                return render_template("index.html",response=response)
+            elif request.json:
+                response = api_response(request)
+                return jsonify(response)
+        except Exception as e:
+            print(e)
+            error = {"error" : "Something Went wrong try again"}
+            return render_template("404.html",error=error)
         pass
     else:
         return render_template("index.html")
