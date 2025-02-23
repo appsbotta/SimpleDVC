bec4988d27a7d5ad455ac254d37f1e22485162e0
Lokesh <appsbotta@gmail.com>
2025-02-23 22:06:48 +0530

    tests added

--- prediction_service/prediction.py
+++ prediction_service/prediction.py
@@ -67,7 +67,7 @@ def form_response(dict_request):
         response = {"The_Expected_Range" : get_schema(),"response":str(e)}
         return response
 
-def api_responce(dict_request):
+def api_response(dict_request):
     try:
         if validate_input(dict_request):
             data = np.array([list(dict_request.values())])
